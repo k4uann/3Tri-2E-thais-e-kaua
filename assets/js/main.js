188bbb92e0b1779796456265ab1a50fcc6edaf7c
k4uann <160138769+k4uann@users.noreply.github.com>
2024-11-05 08:51:27 -0300

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,15 +7,15 @@ function criaCartao(categoria,pergunta,resposta){
       <h3> ${categoria} </h3>
        <div class="cartao-conteudo-pergunta">
       <p> ${pergunta} </p>
-      </div>
-     <div class="cartao-conteudo-resposta">
+       </div>
+      <div class="cartao-conteudo-resposta">
       <p> ${resposta} </p>
       </div>
-      </div>
+    </div>
       `
     container.appendChild(article)
 }
 
 criaCartao('Biologia','Qual a função da mitocôndria?','Criar ATP')
 criaCartao('Piada','Por que o jacaré pai deixou o jacaré filho de castigo?','Porque ele réptil de ano.')
- 
+
